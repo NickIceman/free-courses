abcd6425862a117709fbf1efc960f825eb581081
NickIceman <nicet2011@gmail.com>
2018-05-29 17:49:38 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,3 +1,7 @@
 /* Task 3 */
 
-/* Put your code here */
+let str = 'Hello World 10';
+a = str.replace(/[^qeyuioa]/ig, '');
+b = str.replace(/[^wrtpsdfghjklzxcvbnm]/ig,'');
+c = str.replace(/[^\d]/g,'');
+alert(a + " " + b + " " + c);
