bad1974f7cffcec3ed56a776b63f6fe405d13f6d
NickIceman <nicet2011@gmail.com>
2018-05-29 13:04:44 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,3 +1,22 @@
 /* Task 1 */
 
-/* Put your code here */
+function isPalindrome(str) {
+    let strLen = str.length;
+    let result = '';
+    for (let i = 0; i < strLen; i++) {
+      if (str[i] === str[strLen - 1 - i]) { //Сравниваем все элементы от начала строки со всеми элементами с конца строки
+        result = 'YES';
+      } else {
+        result = 'NO';
+        return result;
+      }
+    }
+    return result;
+  }
+
+  test = isPalindrome('abcdedcba');
+  test2 = isPalindrome('abcded');
+
+  console.log(test);
+  console.log(test2);
+  process.stdout.write('isPalindrome result: ' + 'str - ' + test + ' str2 - ' + test2)
