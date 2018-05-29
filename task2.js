3aa0b2cc3e55903353deb9854a96153e7646279e
NickIceman <nicet2011@gmail.com>
2018-05-29 12:44:22 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,3 +1,26 @@
 /* Task 2 */
 
-/* Put your code here */
+let userNum = +prompt("Введите число от -10000 до 10000 включительно");
+
+if ((userNum > 0) && (userNum <= 10000)) {
+
+    function fib(n) {
+
+        if (n == 1 || n == 2) return 1;
+
+        else return fib(n - 1) + fib(n - 2);
+    }
+
+} else if ((userNum < 0) && (userNum >= -10000)) {
+
+    function fib(n) {
+
+        if (n == 1 || n == 2) return 1;
+
+        else return fib(n + 2) - fib(n + 1);
+    }
+} else if (userNum === 0) {
+    alert(userNum);
+} else alert("Неверный ввод данных!");
+
+process.stdout.write("Fn: " + fib(userNum))
