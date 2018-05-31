641637e8881e1e2007c3e77484a4daf3c47cef4f
NickIceman <nicet2011@gmail.com>
2018-05-31 16:04:31 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,9 +1,6 @@
-/* Task 3 */
-
-//let str = 'Hello World 10';
+'use strict'
 let str = process.argv[1];
 a = str.replace(/[^qeyuioa]/ig, '');
-b = str.replace(/[^wrtpsdfghjklzxcvbnm]/ig,'');
-c = str.replace(/[^\d]/g,'');
-//alert(a + " " + b + " " + c);
+b = str.replace(/[^wrtpsdfghjklzxcvbnm]/ig, '');
+c = str.replace(/[^\d]/g, '');
 process.stdout.write(a + " " + b + " " + c)
