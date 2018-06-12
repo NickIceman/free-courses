a3790bdd4dfa6e06a354caf6db13e36524b16bda
NickIceman <nicet2011@gmail.com>
2018-06-12 16:35:40 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,6 +1,6 @@
 /* Task 3 */
 let str = process.argv[2];
-a = str.replace(/[^qeyuioa]/ig, '');
-b = str.replace(/[^wrtpsdfghjklzxcvbnm]/ig, '');
+a = str.replace(/[^qeyuioaйуеыаоэяию]/ig, '');
+b = str.replace(/[^wrtpsdfghjklzxcvbnmбвгджзйклмнпрстфхцчшщ]/ig, '');
 c = str.replace(/[^\d]/g, '');
 process.stdout.write(a + " " + b + " " + c)
